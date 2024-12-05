4e00de95244faba0e09b7f6f27c862c4362f5f7a
Martin Lončar <49144510+Gureen@users.noreply.github.com>
2024-12-05 12:51:46 +0100

    created class library

---
+++ TournamentTracker/TrackerLibrary/MatchUpEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchUpEntryModel
+    {
+        /// <summary>
+        /// Represent one team in the matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represnts score for this particural team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        ///  Represent the matchup that this team from from
+        ///  as the winner.
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
---
+++ TournamentTracker/TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchUpEntryModel> Entries { get; set; } = new List<MatchUpEntryModel> ();
+        public TeamModel Winner { get; set; }
+        public int MatchupRound { get; set; }
+    }
+}
